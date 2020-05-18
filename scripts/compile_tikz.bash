8ba6c40205b9857cf905f4ec77d98d3c61f45b00
Haschke <haschke@igmr.rwth-aachen.de>
2020-05-18 11:30:12 +0200

    Remove debugging code

--- scripts/compile_tikz.bash
+++ scripts/compile_tikz.bash
@@ -4,9 +4,6 @@ cd tikz
 
 # Set Target Directory
 DIR="../build/tikz"
-#file="tikz/cover_front"
-
-#latexmk -pdf -time -f -output-directory="$DIR" "$file"
 
 # Create target directory if it does not exist yet
 if [ ! -d "$DIR" ]; then
@@ -17,8 +14,6 @@ fi
 for file in *.tex;
 do
   echo "$file"
-  echo ""
-  sleep 2
   latexmk -pdf -time -output-directory="$DIR" "$file"
 done
 
