7d30219a4b84e10df564c0ddcc97cbf80d204036
Haschke <haschke@igmr.rwth-aachen.de>
2020-05-19 12:01:25 +0200

    Rm old scripts in tikz/ directory and add comment for discussion

--- scripts/compile_tikz.bash
+++ scripts/compile_tikz.bash
@@ -15,4 +15,38 @@ do
   latexmk -pdf -time -output-directory="$DIR" -cd "$file"
 done
 
-cd ..
+## Old script from Jascha with additional features that I haven´t understood, yet
+## @Jascha: Why is it necessary to remove .log; .fls and .blg files directly?
+## @Jascha: Is this still necessary when we are using a build directory?
+
+#compileTexFile() {
+#
+#	file=$1
+#
+#	latexmk -pdf -time -silent -output-directory="../build/tikz" "$file"
+#
+#	file_basis=$(basename "$file" .tex)
+#	for ext in "${extlist[@]}"
+#	do
+#		if [ -f "$file_basis$ext" ]
+#		then
+#			echo "Lösche $file_basis$ext"
+#			rm "$file_basis$ext"
+#		fi
+#	done
+#
+#}
+#
+#
+#declare -a extlist=(".log" ".fls" ".blg")
+#
+#echo "Kompiliere alle Bilder"
+#
+#N=16
+#(
+#for file in *.tex
+#do
+#	((i=i%N)); ((i++==0)) && wait
+#	compileTexFile $file &
+#done
+#)
